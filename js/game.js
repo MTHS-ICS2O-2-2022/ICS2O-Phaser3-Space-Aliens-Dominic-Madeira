4354cea1489ffad6c0a2b6de2a0b309527f4a508
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-04-14 17:08:27 +0000

    First Edit

---
+++ js/game.js
@@ -0,0 +1,32 @@
+/* global Phaser */
+
+// Copyright (c) 2023 Dominic M. All rights reserved
+//
+// Created by: Dominic M.
+// Created on: Apr 2023
+// This is the Paser3 game configuration file
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    //we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
