343ee84ad20da5e16ce4e9886ac894f395fc9713
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-02 04:35:48 +0000

    06

--- js/game.js
+++ js/game.js
@@ -8,10 +8,12 @@
 
 import SplashScene from "./splashScene.js"
 import TitleScene from "./titleScene.js"
+import MenuScene from "./menuScene.js"
 
 // Our game scene
 const splashScene = new SplashScene()
 const titleScene = new TitleScene()
+const menuScene = new MenuScene()
 
 //* Game scene */
 const config = {
@@ -38,6 +40,7 @@ const game = new Phaser.Game(config)
 // load scenes
 game.scene.add('splashScene', splashScene)
 game.scene.add('titleScene', titleScene)
+game.scene.add('menuScene', menuScene)
 
 // start title
 game.scene.start ('splashScene')
