4042420fe075f97e22947d26065ac37f7cb3f76d
Dominic-Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2023-05-09 16:44:02 +0000

    08

--- js/gameScene.js
+++ js/gameScene.js
@@ -9,6 +9,9 @@
 class GameScene extends Phaser.Scene {
   constructor () {
     super({ key:'gameScene'})
+
+    this.background = null
+    this.ship = null
   }
 
   /**
@@ -27,6 +30,9 @@ class GameScene extends Phaser.Scene {
    */
   preload () {
     console.log('Game Scene')
+
+    this.load.image('starBackground', './assets/starBackground.png')
+    this.load.image('ship', './assets/spaceShip.png')
   }
 
   /**
@@ -35,7 +41,10 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - Data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create (data) {
-    // pass
+    this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
+    this.background.setOrigin(0, 0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
   }
 
   /**
